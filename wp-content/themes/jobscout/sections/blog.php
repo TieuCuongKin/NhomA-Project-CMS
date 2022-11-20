e5d6f0ac78757e4dd991e3e6e785597b637bafa1
TieuCuongKin <huucuong182@gmail.com>
2022-11-20 20:46:32 +0700

    Blog Job

--- wp-content/themes/jobscout/sections/blog.php
+++ wp-content/themes/jobscout/sections/blog.php
@@ -5,8 +5,7 @@
  * @package JobScout
  */
 
-$blog_heading = get_theme_mod( 'blog_section_title', __( 'Latest Articles', 'jobscout' ) );
-$sub_title    = get_theme_mod( 'blog_section_subtitle', __( 'We will help you find it. We are your first step to becoming everything you want to be.', 'jobscout' ) );
+$blog_heading = get_theme_mod( 'blog_section_title', __( 'NEWEST BLOG ENTRIES', 'jobscout' ) );
 $blog         = get_option( 'page_for_posts' );
 $label        = get_theme_mod( 'blog_view_all', __( 'See More Posts', 'jobscout' ) );
 $hide_author  = get_theme_mod( 'ed_post_author', false );
@@ -16,63 +15,71 @@ $ed_blog      = get_theme_mod( 'ed_blog', true );
 $args = array(
     'post_type'           => 'post',
     'post_status'         => 'publish',
-    'posts_per_page'      => 3,
+    'posts_per_page'      => 4,
     'ignore_sticky_posts' => true
 );
 
 $qry = new WP_Query( $args );
 
-if( $ed_blog && ( $blog_heading || $sub_title || $qry->have_posts() ) ){ ?>
+if( $ed_blog && ( $blog_heading || $qry->have_posts() ) ){ ?>
 <section id="blog-section" class="article-section">
-	<div class="container">
-        <?php 
-            if( $blog_heading ) echo '<h2 class="section-title">' . esc_html( $blog_heading ) . '</h2>';
-            if( $sub_title ) echo '<div class="section-desc">' . wpautop( wp_kses_post( $sub_title ) ) . '</div>'; 
+    <div class="container">
+        <?php
+        if ($blog_heading) {
+            echo '<h2 class="section-title">' . esc_html($blog_heading)
+                . '</h2>';
+        }
         ?>
-        
-        <?php if( $qry->have_posts() ){ ?>
-           <div class="article-wrap">
-    			<?php 
-                while( $qry->have_posts() ){
+
+        <?php if ($qry->have_posts()) { ?>
+            <div class="article-wrap">
+                <?php
+                while ($qry->have_posts()) {
                     $qry->the_post(); ?>
-                    <article class="post">
-        				<figure class="post-thumbnail">
-                            <a href="<?php the_permalink(); ?>" class="post-thumbnail">
-                            <?php 
-                                if( has_post_thumbnail() ){
-                                    the_post_thumbnail( 'jobscout-blog', array( 'itemprop' => 'image' ) );
-                                }else{ 
-                                    jobscout_fallback_svg_image( 'jobscout-blog' ); 
-                                }                            
-                            ?>                        
+                    <article class="post grid-container">
+
+                        <figure class="post-thumbnail grid-item item1">
+                            <a href="<?php the_permalink(); ?>"
+                               class="post-thumbnail">
+                                <?php
+                                if (has_post_thumbnail()) {
+                                    the_post_thumbnail('jobscout-blog',
+                                        array('itemprop' => 'image'));
+                                } else {
+                                    jobscout_fallback_svg_image('jobscout-blog');
+                                }
+                                ?>
                             </a>
                         </figure>
-                        <header class="entry-header">
-                            <div class="entry-meta">
-                                <?php 
-                                    if( ! $hide_author ) jobscout_posted_by(); 
-                                    if( ! $hide_date ) jobscout_posted_on();
-                                ?> 
-                            </div>
-                            <h3 class="entry-title">
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            </h3>
-                        </header>
-        			</article>			
-        			<?php 
+                        <div class="grid-item item2">
+                            <header>
+                                <h3 class="entry-title">
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h3>
+                            </header>
+                            <?php
+                            the_excerpt();
+                            ?>
+                            <!-- .entry-content -->
+                            <?php jobscout_entry_footer(); ?>
+                        </div>
+
+                    </article>
+                    <?php
                 }
                 wp_reset_postdata();
                 ?>
-    		</div><!-- .article-wrap -->
-    		
-            <?php if( $blog && $label ){ ?>
+            </div><!-- .article-wrap -->
+
+            <?php if ($blog && $label) { ?>
                 <div class="btn-wrap">
-        			<a href="<?php the_permalink( $blog ); ?>" class="btn"><?php echo esc_html( $label ); ?></a>
-        		</div>
+                    <a href="<?php the_permalink($blog); ?>"
+                       class="btn"><?php echo esc_html($label); ?></a>
+                </div>
             <?php } ?>
-        
+
         <?php } ?>
-	</div>
+    </div>
 </section>
 <?php 
 }
